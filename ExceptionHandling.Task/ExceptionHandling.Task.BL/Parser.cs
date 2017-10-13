820bba5d5ccb857baddfac4eaa672aa11d3d84c8
V235 <Vi2aly235@gmail.com>
2017-10-13 17:42:23 +0300

    10.13.2017 Exception Handling
    Add Task for module Exception Handling

---
+++ ExceptionHandling.Task/ExceptionHandling.Task.BL/Parser.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Task.BL
+{
+    public class Parser
+    {
+        public char GetFirstSymbol(string str)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(str))
+                    throw new ArgumentException("incorrect input data");
+                return str[0];
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private int ConvertCharToInt(char c)
+        {
+            return c - '0';
+        }
+
+        private void Negative(char c, ref bool isNegative, ref int start)
+        {
+            switch (c)
+            {
+                case '-':
+                {
+                    isNegative = true;
+                    start = 1;
+                    break;
+                }
+                case '+':
+                {
+                    isNegative = false;
+                    start = 1;
+                    break;
+                }
+
+            }
+        }
+        public int ConvertStringToInt(string str)
+        {
+            bool isNegative=false;
+            int start = 0;
+            try
+            {
+                if (string.IsNullOrEmpty(str))
+                    throw new ArgumentException();
+                if (str.Length > 1)
+                {
+                    Negative(str[0], ref isNegative, ref start);
+                }
+                int result = 0;
+                for (int i = start; i < str.Length; i++)
+                {
+                    if (str[i] < '0' || str[i] > '9')
+                    {
+                        throw new ArgumentException();
+                    }
+                    result = checked(result * 10 + ConvertCharToInt(str[i]));
+                }
+                return isNegative ? -result : result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ExceptionHandling.Task/ExceptionHandling.Task/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExceptionHandling.Task.BL;
+
+namespace ExceptionHandling.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser();
+            while (true)
+            {
+                Console.WriteLine("Chose option:");
+                Console.WriteLine("Get first symbol from string (press 1)");
+                Console.WriteLine("Convert string to int (press 2)");
+                string choose = Console.ReadLine();
+                switch (choose)
+                {
+                    case "1":
+                        Console.Write("Enter string:");
+                        GetFirstSymbolFromInputString(parser, Console.ReadLine());
+                        break;
+                    case "2":
+                        Console.Write("Enter number:");
+                        ConvertStringToIntFromInputString(parser, Console.ReadLine());
+                        break;
+
+                }
+            }
+        }
+
+        private static void GetFirstSymbolFromInputString(Parser parser, string str)
+        {
+            try
+            {
+                Console.WriteLine("You have entered:{0}", parser.GetFirstSymbol(str));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void ConvertStringToIntFromInputString(Parser parser, string str)
+        {
+            try
+            {
+                Console.WriteLine("You have entered:{0}", parser.ConvertStringToInt(str));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
