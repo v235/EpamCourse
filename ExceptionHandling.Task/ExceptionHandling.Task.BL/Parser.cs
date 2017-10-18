ccb3370f46834de556bc08ba7fb5f650bfd60211
V235 <Vi2aly235@gmail.com>
2017-10-18 11:43:55 +0300

    code refract

--- ExceptionHandling.Task/ExceptionHandling.Task.BL/Parser.cs
+++ ExceptionHandling.Task/ExceptionHandling.Task.BL/Parser.cs
@@ -11,16 +11,9 @@ namespace ExceptionHandling.Task.BL
     {
         public char GetFirstSymbol(string str)
         {
-            try
-            {
-                if (string.IsNullOrEmpty(str))
-                    throw new ArgumentException("incorrect input data");
-                return str[0];
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            if (string.IsNullOrEmpty(str))
+                throw new ArgumentException("incorrect input data");
+            return str[0];
         }
 
         private int ConvertCharToInt(char c)
@@ -47,33 +40,27 @@ namespace ExceptionHandling.Task.BL
 
             }
         }
+
         public int ConvertStringToInt(string str)
         {
-            bool isNegative=false;
+            bool isNegative = false;
             int start = 0;
-            try
+            if (string.IsNullOrEmpty(str))
+                throw new ArgumentException();
+            if (str.Length > 1)
             {
-                if (string.IsNullOrEmpty(str))
+                Negative(str[0], ref isNegative, ref start);
+            }
+            int result = 0;
+            for (int i = start; i < str.Length; i++)
+            {
+                if (str[i] < '0' || str[i] > '9')
+                {
                     throw new ArgumentException();
-                if (str.Length > 1)
-                {
-                    Negative(str[0], ref isNegative, ref start);
                 }
-                int result = 0;
-                for (int i = start; i < str.Length; i++)
-                {
-                    if (str[i] < '0' || str[i] > '9')
-                    {
-                        throw new ArgumentException();
-                    }
-                    result = checked(result * 10 + ConvertCharToInt(str[i]));
-                }
-                return isNegative ? -result : result;
+                result = checked(result * 10 + ConvertCharToInt(str[i]));
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            return isNegative ? -result : result;
         }
     }
 }
