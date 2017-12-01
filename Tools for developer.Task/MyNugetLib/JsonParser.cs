4c2873c4abd0492037d1a4fd4940306f3aa695e7
V235 <Vi2aly235@gmail.com>
2017-12-01 17:34:18 +0300

    add nuget

---
+++ Tools for developer.Task/MyNugetLib/JsonParser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+
+
+
+namespace MyNugetLib
+{
+    public class JsonParser
+    {
+        public IEnumerable<string> ParseJson(string json)
+        {
+            return JArray.Parse(json).Select(s=>s.ToString());
+        }
+        public string GetFirstElement(string json)
+        {
+            return JArray.Parse(json).Select(s => s.ToString()).FirstOrDefault();
+        }
+
+        public string FindElementByKey(string json, string key)
+        {
+            return JArray.Parse(json).Select(s => s.ToString()).First(e=>e==key);
+        }
+    }
+}
---
+++ Tools for developer.Task/MyNugetLib/package.bat
@@ -0,0 +1,1 @@
+nuget pack MyNugetLib.csproj -Properties Configuration=Release -Build -OutputDirectory .\Bin 
