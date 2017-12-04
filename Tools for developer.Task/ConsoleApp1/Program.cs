6a35c953cf7b9e87a8cd40a1a4b0bc7a1ee5fae7
V235 <Vi2aly235@gmail.com>
2017-12-04 17:02:38 +0300

    change

---
+++ Tools for developer.Task/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyNugetLib;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = @"['Small','Medium','Large']";
+            JsonParser js = new JsonParser();
+            var r = js.ParseJson(json);
+            var r1 = js.GetFirstElement(json);
+            var r2 = js.FindElementByKey(json, "Medium");
+            Console.WriteLine("{0},{1}", r1, r2);
+        }
+    }
+}
